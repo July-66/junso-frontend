23ad83025fb8529ccad3333d63d118c8e61b7a32
Cheese <1430119384@qq.com>
2023-04-04 00:55:45 +0800

    junso

---
+++ src/components/UserList.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-list
+    item-layout="horizontal"
+    :grid="{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 3 }"
+    :data-source="props.userList"
+  >
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <a-card hoverable style="width: 240px">
+          <template #cover>
+            <img alt="example" :src="item.useravatar" />
+          </template>
+          <a-card-meta :title="item.username">
+            <template #description>{{ item.userprofile }}</template>
+          </a-card-meta>
+        </a-card>
+      </a-list-item>
+    </template>
+  </a-list>
+</template>
+
+<script setup lang="ts">
+import { withDefaults, defineProps } from "vue";
+
+interface Props {
+  userList: any[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  userList: () => [],
+});
+</script>
+
+<style scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+module.exports = {
+  devServer: {
+    port: 8101,
+    proxy: {
+      "/api": {
+        // 匹配所有以 '/api'开头的请求路径
+        target: "http://43.139.7.202:8102", // 代理目标的基础路径
+        changeOrigin: true, //跟服务端说谎，说自己是跟服务端同一域名的
+      },
+    },
+  },
+};
